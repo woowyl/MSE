07b987ad11dfcdf4a2b99ccb2b1b6d83e0ede973
woowyl <woowyl@gmail.com>
2020-08-11 23:48:02 +0800

    ip限制

--- 20-1/python/douban_movie/scraw.py
+++ 20-1/python/douban_movie/scraw.py
@@ -10,13 +10,14 @@ import bs4
 import os.path
 from fake_useragent import UserAgent  
 
-ua = UserAgent(use_cache_server=False) 
+#ua = UserAgent(use_cache_server=False) 
+ua = UserAgent(verify_ssl=False) 
 print(ua.random)
 APIEND = 20000
 
 def get_ip_list():
     #国内高匿代理ip网站
-    url = 'https://www.kuaidaili.com/free/intr/4/'
+    url = 'https://www.kuaidaili.com/free/intr/5/'
     ipAgent = {
         'http':'https://60.216.20.211:8001'
     }
@@ -48,6 +49,7 @@ def get_page(url):
     querystring = {"status":"P"}
     headers = {
         'user-agent': ua.random,
+        'Cookie': 'll="108296"; bid=yCQKy1Zfre0; __utmz=30149280.1590235049.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _vwo_uuid_v2=DB73CB88E1A6E82140889364A5414874F|9b7e19408b6120c2eeb607301bf50be5; __utmc=30149280; __utmc=223695111; push_noty_num=0; push_doumail_num=0; __utmv=30149280.8848; ap_v=0,6.0; __utma=30149280.607930696.1590235049.1597073781.1597160293.7; __utmt=1; __utma=223695111.1743980471.1590235050.1597073781.1597160295.6; __utmz=223695111.1597160295.6.4.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1597160295%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_ses.100001.4cf6=*; _pk_id.100001.4cf6=9d6e287a985fe453.1590235050.6.1597160317.1597073780.; __utmb=223695111.4.10.1597160295; dbcl2="88489827:i32rzHzDTUI"; ck=db7p; __utmb=30149280.8.10.1597160293'
     }
     proxies = get_random_ip(ipList)
     # 加代理版本
@@ -55,6 +57,7 @@ def get_page(url):
     response = requests.request("GET", url, headers=headers, params=querystring, proxies = proxies)
     #response = requests.get(url=url,headers=headers)
     data = response.text
+    print(data);
     return data
 
 # 解析网页源代码
@@ -120,9 +123,6 @@ def crawlAPI(start):
     print('结束爬取')
 
 # 爬取详情页
-
-
-
 if __name__ == '__main__':
-    crawlAPI(2240)
+    crawlAPI(2460)
     #print(list(map(getAPIUrl,[{'a':1, "url":'hello'},{"url":'world'}])))
